6265556cc595cdabcda6dd2edc9b0ed24308a300
Jesse White <jwhite08@gmail.com>
2013-07-13 14:22:18 -0400

    Initial prototype of camel-eventd extensions.

---
+++ features/clustering/eventd/src/main/java/org/opennms/netmgt/eventd/adaptors/CamelEventReceiver.java
@@ -0,0 +1,136 @@
+package org.opennms.netmgt.eventd.adaptors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.opennms.netmgt.xml.event.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CamelEventReceiver implements EventReceiver {
+    private List<EventHandler> m_eventHandlers = new ArrayList<EventHandler>(3);
+    private static final Logger LOG = LoggerFactory.getLogger(CamelEventReceiver.class);
+
+    /**
+     * Camel context.
+     */
+    private CamelContext m_context;
+
+    /**
+     * Input.
+     */
+    private static final String ROUTE_INPUT_URI = "netty:udp://224.2.2.3:5818/";
+
+    /**
+     * Output.
+     */
+    private static final String ROUTE_ENDPOINT_URI = "bean:camelEventReceiver?method=onEventReceived";
+
+    /**
+     * The fiber's status.
+     */
+    private volatile int m_status;
+
+    @Override
+    public void init() {
+        m_status = START_PENDING;
+
+        try {
+            m_context = new DefaultCamelContext();
+            m_context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() {
+                    from(ROUTE_INPUT_URI).to(ROUTE_ENDPOINT_URI);
+                }
+            });
+        } catch (Exception e) {
+            LOG.error("Initialization the context failed", e);
+            m_context = null;
+        }
+    }
+
+    @Override
+    public synchronized void start() {
+        if (m_status != START_PENDING) {
+            return;
+        }
+
+        m_status = STARTING;
+
+        try {
+            m_context.start();
+        } catch (Exception e) {
+            LOG.error("Error while starting the context", e);
+            m_status = STOPPED;
+        }
+
+        m_status = RUNNING;
+    }
+
+    @Override
+    public synchronized void stop() {
+        if (m_status != RUNNING) {
+            return;
+        }
+
+        m_status = STOP_PENDING;
+
+        try {
+            m_context.stop();
+        } catch (Exception e) {
+            LOG.error("Error occured while stopping the context", e);
+        }
+
+        m_status = STOPPED;
+    }
+
+    @Override
+    public synchronized int getStatus() {
+        return m_status;
+    }
+    
+    @Override
+    public String getName() {
+        return "Event Camel Receiver[" + ROUTE_INPUT_URI + "]";
+    }
+
+    @Override
+    public void destroy() {
+        // This method is intentionally left blank
+    }
+
+    public void onEventReceived(Event event) {
+        synchronized (m_eventHandlers) {
+            // Pass the event to all of the event handlers
+            for (final EventHandler eventHandler : m_eventHandlers) {
+                try {
+                    LOG.debug("Handling event: {}", event);
+                    eventHandler.processEvent(event);
+                } catch (final Throwable t) {
+                    LOG.warn("An exception occured while processing an event.", t);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void addEventHandler(EventHandler handler) {
+        synchronized (m_eventHandlers) {
+            if (!m_eventHandlers.contains(handler)) {
+                LOG.debug("Adding event handler {}", handler);
+                m_eventHandlers.add(handler);
+            }
+        }
+    }
+
+    @Override
+    public void removeEventHandler(EventHandler handler) {
+        synchronized (m_eventHandlers) {
+            LOG.debug("Removing event handler {}", handler);
+            m_eventHandlers.remove(handler);
+        }
+    }
+}
---
+++ features/clustering/eventd/src/main/java/org/opennms/netmgt/eventd/processor/CamelEventBroadcaster.java
@@ -0,0 +1,73 @@
+package org.opennms.netmgt.eventd.processor;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.opennms.netmgt.model.events.EventProcessor;
+import org.opennms.netmgt.xml.event.Event;
+import org.opennms.netmgt.xml.event.Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class CamelEventBroadcaster implements EventProcessor,
+        InitializingBean, DisposableBean {
+    /**
+     * Camel context.
+     */
+    private CamelContext m_context;
+
+    /**
+     * Camel producer used to inject events.
+     */
+    private ProducerTemplate m_producer;
+
+    /**
+     * Input.
+     */
+    private static final String ROUTE_INPUT_URI = "direct:event";
+
+    /**
+     * Output.
+     */
+    private static final String ROUTE_ENDPOINT_URI = "netty:udp://224.2.2.3:5818/?broadcast=true";
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(CamelEventBroadcaster.class);
+
+    @Override
+    public void process(Header eventHeader, Event event) {
+        if (event.getLogmsg() != null
+                && event.getLogmsg().getDest().equals("suppress")) {
+            LOG.debug("process: skip sending event {} to other daemons because is marked as suppress",
+                      event.getUei());
+        } else {
+            LOG.debug("process: sending event {}", event);
+            m_producer.sendBody("direct:event", event);
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        m_context = new DefaultCamelContext();
+        m_context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from(ROUTE_INPUT_URI).to(ROUTE_ENDPOINT_URI);
+            }
+        });
+        m_context.start();
+        m_producer = m_context.createProducerTemplate();     
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        if (m_context != null) {
+            m_context.stop();
+        }
+    }
+}
